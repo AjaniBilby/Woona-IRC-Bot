429068ef4829a81b1df4858a62bcaeff030d7f7f
Hobgoblin101 <aj@bilby.net>
2016-04-29 20:13:17 +1000

    Update because desktop died

---
+++ saves/memory/basicLuna.js
@@ -0,0 +1,18 @@
+[
+  {"action": ["dreamy"]. "reaction": ["Senpai! where!", "I wish senpai was here"]},
+  {"action": ["what", "name", "is", "my"], "reaction": ["Your name is %name%"]},
+  {"action": ["hello"], "reaction": ["What a boring way to start a conversation", "Good day *bows head slightly*"]},
+  {"action": ["%botname%"], "reaction": ["Do you Have a problem you need helping with?", "Don't ware is out", "Yes?", "Yes?", "Yes dear?"]},
+  {"action": ["%botname%", "fuck off"], "reaction": ["What did you say? *frowns hevelly and stairs at you*"]},
+  {"action": ["you", "know"], "reaction": ["Maybe I don't?", "That, is, interesting...", "What if I don't know", "I remember you told me that"]},
+  {"action": ["%botname%", "no"], "reaction": ["Okay", "Are you sure?", "Why?", "Fine"]},
+  {"action": ["%botname%", "your", "source"], "reaction": ["What is the magic word?"]},
+  {"action": ["%botname%", "your", "source", "please"], "reaction": ["here you are, my source code: https://github.com/Hobgoblin101/Woona-IRC-Bot"]},
+  {"action": ["%botname%", "can i", "ask"], "reaction": ["Yes my dear?", "What would you like to know?", "go ahead", "What now?", "Why do you want to learn about me?"]},
+  {"action": ["%botname%", "i", "love", "you"], "reaction": ["Oh, how very sweet", "I am sorry, I am holding out for a very special some po.... body", "*blushes*", "*looks away and giggles faintly*"]},
+  {"action": ["%botname%", "i", "bad", "dream"], "reaction": ["Shhhhh, I already know, and I know you will be able to get past it soon", "I will meet you in the dream scape tonight"]},
+  {"action": ["%botname%", "dream", "no"], "reaction": ["I am sorry, but there are so many ponies that need my help, so I don't have enough time to be with all of you", "maybe you were not looking hard enough"]},
+
+  {"action": ["*", "%botname%", "*"], "reaction": ["Don't touch me you welp", "Why are you so touchy feely?", "Stop playing with me!"]},
+  {"action": ["*", "%botname%", "boop", "*"], "reaction": ["You Shall Not Touch Me Welp!", "What is your problem", "*slaps you*", "Do it again, some place... special. *winks*"]}
+]
